94c53dda8aaede53f0e59fde3959c7d3679c6045
buckydebugs <73716542+buckydebugs@users.noreply.github.com>
2025-04-02 00:14:16 +0530

    Create Easy_Binary_Search_Recursive.cpp

---
+++ Binary Search/Easy_Binary_Search_Recursive.cpp
@@ -0,0 +1,62 @@
+/*
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+Example 2:
+
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 < nums[i], target < 104
+All the integers in nums are unique.
+nums is sorted in ascending order.
+*/
+class Solution {
+public:
+
+    int helper(vector<int> &nums, int low, int high, int target)
+    {
+        if(low > high)
+        {
+            return -1;
+        }
+        int mid = (low+high)/2;
+        if(nums[mid] == target)
+        {
+            return mid;
+        }
+        if(nums[mid] < target)
+        {
+            return helper(nums, mid + 1, high, target);
+        }
+        else
+        {
+            return helper(nums, low, mid - 1, target);
+        } 
+        
+    }
+    int search(vector<int>& nums, int target) 
+    {
+        int n = nums.size();
+        int low = 0;
+        int high = n - 1;
+        
+        return helper(nums, low, high, target);
+    }
+
+};
+//T.C. => O(log2 N)
+//S.C. => O(1)

5313e20fca6fb8a39dd3a602b0ac2038a9ec248a
buckydebugs <73716542+buckydebugs@users.noreply.github.com>
2025-05-02 02:14:53 +0530

    Create Medium_Find_Kth_Rotation_Optimal.cpp

---
+++ Binary Search/Medium_Find_Kth_Rotation_Optimal.cpp
@@ -0,0 +1,77 @@
+/*
+Given an increasing sorted rotated array arr of distinct integers. The array is right-rotated k times. Find the value of k.
+Let's suppose we have an array arr = [2, 4, 6, 9], so if we rotate it by 2 times so that it will look like this:
+After 1st Rotation : [9, 2, 4, 6]
+After 2nd Rotation : [6, 9, 2, 4]
+
+Examples:
+
+Input: arr = [5, 1, 2, 3, 4]
+Output: 1
+Explanation: The given array is 5 1 2 3 4. The original sorted array is 1 2 3 4 5. We can see that the array was rotated 1 times to the right.
+Input: arr = [1, 2, 3, 4, 5]
+Output: 0
+Explanation: The given array is not rotated.
+Expected Time Complexity: O(log(n))
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 <= n <=105
+1 <= arri <= 107
+
+
+*/
+
+// User function template for C++
+class Solution {
+  public:
+
+int findKRotation(vector<int> &arr) {
+    int low = 0, high = arr.size() - 1;
+    int ans = INT_MAX;
+    int index = -1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        //search space is already sorted
+        //then arr[low] will always be
+        //the minimum in that search space:
+        if (arr[low] <= arr[high]) {
+            if (arr[low] < ans) {
+                index = low;
+                ans = arr[low];
+            }
+            break;
+        }
+
+        //if left part is sorted:
+        if (arr[low] <= arr[mid]) {
+            // keep the minimum:
+            if (arr[low] < ans) {
+                index = low;
+                ans = arr[low];
+            }
+
+            // Eliminate left half:
+            low = mid + 1;
+        }
+        else { //if right part is sorted:
+
+            // keep the minimum:
+            if (arr[mid] < ans) {
+                index = mid;
+                ans = arr[mid];
+            }
+
+            // Eliminate right half:
+            high = mid - 1;
+        }
+    }
+    return index;
+}
+
+
+
+};
+// for unique elements 
+// T.C. => O(log2n)
+// S.C. => O(1)

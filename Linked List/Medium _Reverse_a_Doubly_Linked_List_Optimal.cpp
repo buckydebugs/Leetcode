198d95b962d138899939c7724cb28c00a98d32b7
buckydebugs <73716542+buckydebugs@users.noreply.github.com>
2025-05-20 12:58:05 +0530

    Create Medium _Reverse_a_Doubly_Linked_List_Optimal.cpp

---
+++ Linked List/Medium _Reverse_a_Doubly_Linked_List_Optimal.cpp
@@ -0,0 +1,60 @@
+/*
+  Given a doubly linked list. Your task is to reverse the doubly linked list and return its head.
+
+Examples:
+
+Input: LinkedList: 3 <-> 4 <-> 5
+Output: 5 <-> 4 <-> 3
+
+Input: LinkedList: 75 <-> 122 <-> 59 <-> 196
+Output: 196 <-> 59 <-> 122 <-> 75
+
+Expected Time Complexity: O(n).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= number of nodes <= 106
+0 <= node->data <= 104
+  */
+
+
+/*
+class DLLNode {
+  public:
+    int data;
+    DLLNode *next;
+    DLLNode *prev;
+
+    DLLNode(int val) {
+        data = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    // Function to reverse a doubly linked list
+    DLLNode* reverseDLL(DLLNode* head) 
+    {
+        if(head == NULL || head->next == NULL)
+        {
+            return head;
+        }
+        DLLNode* current = head;
+        DLLNode* last = NULL;
+        while(current != NULL)
+        {
+            last = current->prev;
+            current->prev = current->next;
+            current->next = last;
+            current = current->prev;
+        }
+        return last->prev;
+    }
+        
+    
+};
+
+//T.C. -> O(N)
+//S.C. -> O(1)
